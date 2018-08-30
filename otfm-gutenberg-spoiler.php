c6a28ae26d0d0fbe5668000b755b1d623a310e12
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2018-08-30 12:30:48 +0400

    reorganization blocks

--- otfm-gutenberg-spoiler.php
+++ otfm-gutenberg-spoiler.php
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) exit; // Game over
 function ogs_script(){
     wp_register_script(
             'ogs_script',
-            plugins_url('js/otfm-gutenberg-spoiler.js', __FILE__),
+            plugins_url('blocks/little-spoiler/index.js', __FILE__),
             array( 'wp-blocks', 'wp-i18n', 'wp-element' )
     );
 
@@ -33,7 +33,7 @@ add_action('init', 'ogs_script');
 function ogs_spoiler_script(){
     wp_enqueue_script(
             'otfm-guten-spoiler-js',
-            plugins_url('js/otfm-spoiler.js', __FILE__),
+            plugins_url('frontend/otfm-spoiler.js', __FILE__),
             array( 'jquery' ),
             '',
             true
@@ -46,7 +46,7 @@ add_action( 'wp_enqueue_scripts', 'ogs_spoiler_script' );
 function ogs_spoiler_style(){
     wp_enqueue_style(
             'otfm-guten-spoiler-css',
-            plugins_url('css/otfm-spoiler.css', __FILE__)
+            plugins_url('frontend/otfm-spoiler.css', __FILE__)
     );
 }
 add_action( 'wp_enqueue_scripts', 'ogs_spoiler_style' );
